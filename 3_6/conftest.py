fa08e3de6ef7bb84c44fb2f6f6623e9dcac50e14
itkacheva <itkacheva374@gmail.com>
2023-04-04 14:45:27 +0300

    Update files

--- 3_6/conftest.py
+++ 3_6/conftest.py
@@ -4,15 +4,17 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language')
+    parser.addoption('--language', action='store', default='en')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
+    browser.get(link)
     yield browser
     print("\nquit browser..")
     browser.quit()
